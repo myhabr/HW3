b36ecb6432b480ad458fce3baa7a9625391f9d5d
Ramus Aleksei <myhabr@gmail.com>
2017-10-30 13:40:51 +1000

    Исправление замечаний

--- task3_excep_files_var6.py
+++ task3_excep_files_var6.py
@@ -1,12 +1,14 @@
 import re
-stroka = ''
+
+
+str1 = ''
 
 def analize_text(str1):
     str_low = str1.lower()  # приводим строку в lower_case
     reg = re.compile('[^a-zа-я]')
-    str_obrez = reg.sub('', str_low)  # оставляем только буквы RUS|ENG алфавита, исключая спец.символы и пробелы
-    key = set(str_obrez)  # формируем множество уникальных букв из строки
-    values = (str_obrez.count(letter) for letter in key)  # считаем частоту встречаемости буквы в строке str_obrez
+    str_cut = reg.sub('', str_low)  # оставляем только буквы RUS|ENG алфавита, исключая спец.символы и пробелы
+    key = set(str_cut)  # формируем множество уникальных букв из строки
+    values = (str_cut.count(letter) for letter in key)  # считаем частоту встречаемости буквы в строке str_obrez
     analize = dict(zip(key, values))  # используем zip, объединяем в пары и создаем словарь
     str_out = ''
     for letter_d, frequency_d in analize.items():
@@ -14,14 +16,9 @@ def analize_text(str1):
     return str_out
 
 try:
-    f = open('orig_text.txt', 'rt')
-    chunk = 2000  # читаю буфером
-    while True:
-        fragment = f.read(chunk)
-        if not fragment:
-            break
-        stroka += fragment  # формируем итоговую строку
-    f.close()
+    f = open('orig_text.txt')
+    str1 = f.read()
+    f.close
 except FileNotFoundError:
     print("Файл orig_text.txt не найден")
     exit()
@@ -30,7 +27,7 @@ except FileNotFoundError:
 #  защитим его от перезаписи в случае, если он существует.
 try:
     with open('output_analize.txt', 'xt') as fout:
-        fout.write(analize_text(stroka))
+        fout.write(analize_text(str1))
 except FileExistsError:
     print("Файл output_analize.txt уже существует")
     exit()
